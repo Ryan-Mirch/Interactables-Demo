6332899aa926e0866020ddd35da08f3ae5084152
Xananax <me@xananax.com>
2022-11-16 18:41:12 +0100

    chore: refactor, documentation

--- Interactable/Interactable.gd
+++ Interactable/Interactable.gd
@@ -4,32 +4,43 @@ class_name Interactable extends Area3D
 func _ready() -> void:
 	connect("body_entered",_on_body_entered)
 	connect("body_exited",_on_body_exited)
-	add_to_group("Interactable")
 
 
-func _on_body_entered(body: Node3D) -> void:
-	InteractableManager.add_in_range_interactable(self)
+func _on_body_entered(_body: PhysicsBody3D) -> void:
+	InteractableManager.add_interactable_in_range(self)
 
 
-func _on_body_exited(body: Node3D) -> void:
-	InteractableManager.erase_in_range_interactable(self)
-	
-
-func is_in_range() -> bool:
-	return has_overlapping_bodies()
+func _on_body_exited(_body: PhysicsBody3D) -> void:
+	InteractableManager.erase_interactable_in_range(self)
 
 
-#Following functions are implemented in the interactable's script
+# Below are functions meant to be overridden in subclasses
+
+
+# Returns true if the object is currently interactable.
+# Returns `true` by default, override in sub-classes to change the behavior.
+# @returns `true` if the interactable is ready to be interacted with, `false` 
+#           otherwise. Specifics are left to the discretion of subclasses
 func is_interactable() -> bool:
 	return true
 
 
-func interacted() -> void:
-	print("Interacted!")
-	
-	
+# Triggers the interaction. Override this in sub-classes to change the behavior.
+# What this does depends on each specific subclass implementation
+func trigger_interaction() -> void:
+	printerr("This is an abstract method destined to be overridden in subclasses")
+
+
+# Gets the interaction name. This is used to display a prompt to the user.
+# @returns the text to write to prompt the user to interact
 func get_prompt_text() -> String:
 	return "Prompt"
-	
-func highlight(value:bool) -> void:
-	print("Highlighting")
+
+
+# Highlights the interactable. This cannot be done generically because it is going.
+# to depend on the specific interactable.
+# Override in sub-classes to change the behavior.
+# @param is_highlighted if true, highlights the interactable. Behavior is left 
+#                       at the discretion of the subclass implementation.
+func highlight(_is_highlighted: bool) -> void:
+	printerr("This is an abstract method destined to be overridden in subclasses")
