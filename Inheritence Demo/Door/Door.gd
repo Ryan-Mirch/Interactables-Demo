fd4ed9ac5256e4f1ae64a98382f057838a14385b
Ryan Mirch <rwmirch@gmail.com>
2022-11-23 20:20:48 -0500

    Refactor: Changed file structure to allow multiple godot projects in the repo

---
+++ Inheritence Demo/Door/Door.gd
@@ -0,0 +1,48 @@
+extends Interactable
+
+
+@onready var _animation_player: AnimationPlayer = $AnimationPlayer
+@onready var _highlighter: Label3D = $Highlight
+
+# Tracks the door's state. Changes at the end of an animation
+var _is_open = false
+
+
+func _ready() -> void:
+	super._ready()
+	_animation_player.connect("animation_finished", _on_animation_player_animation_finished)
+
+
+# Triggers the interaction. Toggles the door open or closed
+func trigger_interaction() -> void:	
+	if _is_open:
+		_animation_player.play("Close")
+	else:
+		_animation_player.play("Open")
+	InteractableManager.update()
+
+
+# Returns `true` if no animation is currently playing
+func is_interactable() -> bool:
+	return !_animation_player.is_playing()
+
+
+# Shows or hides the highlight dot
+func highlight(is_highlighted: bool) -> void:
+	_highlighter.visible = is_highlighted
+
+
+# Gets the interaction name. This is used to display a prompt to the user.
+# @returns the text to write to prompt the user to interact
+func get_prompt_text() -> String:
+	if _is_open:
+		return "Close Door"
+	else:
+		return "Open Door"
+
+
+func _on_animation_player_animation_finished(animation_name: String) -> void:
+	if animation_name == "RESET":
+		return
+	_is_open = animation_name == "Open"
+	InteractableManager.update()
---
+++ Inheritence Demo/Interactable/Interactable.gd
@@ -0,0 +1,75 @@
+class_name Interactable extends Area3D
+
+#Used for calculating line of sight
+@export var center_position:Marker3D
+@export var hitboxes:Array[NodePath]
+
+var player:CharacterBody3D = null
+
+func _ready() -> void:
+	connect("body_entered",_on_body_entered)
+	connect("body_exited",_on_body_exited)
+	collision_layer = 0
+	collision_mask = 2
+
+
+func _on_body_entered(body: CharacterBody3D) -> void:
+	player = body
+	InteractableManager.add_interactable_in_range(self)
+	
+
+func _on_body_exited(_body: CharacterBody3D) -> void:
+	player = null
+	InteractableManager.erase_interactable_in_range(self)	
+	
+
+func player_is_in_line_of_sight() -> bool:
+	if player != null:
+		var space_state := get_world_3d().direct_space_state
+		
+		var exclude = []
+		for nodePath in hitboxes:
+			exclude.append(get_node(nodePath))
+		
+		var params = PhysicsRayQueryParameters3D.create(
+		center_position.global_position, 
+		player.global_position, 
+		player.collision_mask, 
+		exclude)
+		
+		var result := space_state.intersect_ray(params)
+		if result == {}:
+			return true
+			
+	return false
+
+# Below are functions meant to be overridden in subclasses
+
+
+# Returns true if the object is currently interactable.
+# Returns `true` by default, override in sub-classes to change the behavior.
+# @returns `true` if the interactable is ready to be interacted with, `false` 
+#           otherwise. Specifics are left to the discretion of subclasses
+func is_interactable() -> bool:
+	return true
+
+
+# Triggers the interaction. Override this in sub-classes to change the behavior.
+# What this does depends on each specific subclass implementation
+func trigger_interaction() -> void:
+	printerr("This is an abstract method destined to be overridden in subclasses")
+
+
+# Gets the interaction name. This is used to display a prompt to the user.
+# @returns the text to write to prompt the user to interact
+func get_prompt_text() -> String:
+	return "Prompt"
+
+
+# Highlights the interactable. This cannot be done generically because it is going.
+# to depend on the specific interactable.
+# Override in sub-classes to change the behavior.
+# @param is_highlighted if true, highlights the interactable. Behavior is left 
+#                       at the discretion of the subclass implementation.
+func highlight(_is_highlighted: bool) -> void:
+	printerr("This is an abstract method destined to be overridden in subclasses")
