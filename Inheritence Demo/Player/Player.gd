a15513cb7efc6c265a2e70cfb0e71d98f3122691
Ryan Mirch <rwmirch@gmail.com>
2022-12-01 15:55:26 -0500

    Inheritance demo now uses closest to player

--- Inheritence Demo/Player/Player.gd
+++ Inheritence Demo/Player/Player.gd
@@ -6,6 +6,8 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready() -> void:
+	InteractableManager.player = self
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
