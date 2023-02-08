a8705a146ba0b291450c94c1dcd635b52ec1b674
Xananax <me@xananax.com>
2023-02-08 18:55:50 +0400

    chore: remove scene signals, replace with in-code signals
    also some formatting changes

--- Simple Demo/button/button.gd
+++ Simple Demo/button/button.gd
@@ -1,20 +1,17 @@
 extends Node3D
 
-@onready var area := $Area3D
-@onready var animation_player := $AnimationPlayer
-@onready var prompt := $Prompt
+@onready var area: Area3D = $Area3D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var prompt: Label3D = $Prompt
 
 var player:CharacterBody3D
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
+	area.body_entered.connect(_on_area_3d_body_entered)
+	area.body_exited.connect(_on_area_3d_body_exited)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
 func _input(event: InputEvent) -> void:
 	#player is NOT in range
 	if player == null: return
@@ -22,9 +19,8 @@ func _input(event: InputEvent) -> void:
 	#player is in range
 	if event.is_action_pressed("interact") and !animation_player.is_playing():
 		_interacted()
-		
-	
-	
+
+
 func _interacted():
 	animation_player.play("Press")
 
@@ -32,9 +28,8 @@ func _interacted():
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	player = body
 	prompt.visible = true
-	
 
 
-func _on_area_3d_body_exited(body: Node3D) -> void:
+func _on_area_3d_body_exited(_body: Node3D) -> void:
 	player = null
 	prompt.visible = false
